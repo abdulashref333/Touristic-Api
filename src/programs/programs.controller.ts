df40bf51509d7451e2c60a52ad249bef4bdc943d
Abdulrahman <abdul.ashref333@gmail.com>
2022-06-29 20:39:40 +0200

    add swagger api decorators

--- src/programs/programs.controller.ts
+++ src/programs/programs.controller.ts
@@ -7,23 +7,35 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateProgramDto } from './dto/create.program.dto';
 import { UpdateProgramDto } from './dto/update.program.dto';
 import IProgram from './programs.interface';
 
 import { ProgramService } from './programs.service';
 
+@ApiTags('Programs')
+@ApiBearerAuth()
 @Controller('programs')
 export class ProgramController {
   constructor(private readonly programService: ProgramService) {}
 
   @Post()
+  @ApiBody({ type: CreateProgramDto })
+  @ApiOperation({ summary: 'Create Program' })
+  @ApiResponse({ status: 400, description: 'BadRequest.' })
   async create(@Body() createProgramDto: CreateProgramDto): Promise<IProgram> {
-    console.log(createProgramDto);
     return this.programService.create(createProgramDto);
   }
 
   @Get('/count')
+  @ApiOperation({ summary: 'Get Programs Count' })
   async count(): Promise<number> {
     return await this.programService.count();
   }
@@ -35,11 +47,14 @@ export class ProgramController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete Program' })
   async remove(@Param('id') id: string): Promise<IProgram> {
     return this.programService.remove(id);
   }
 
   @Patch(':id')
+  @ApiBody({ type: UpdateProgramDto })
+  @ApiOperation({ summary: 'Update Program' })
   async update(
     @Param('id') id: string,
     @Body() updateProgramDto: UpdateProgramDto,
